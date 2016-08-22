43458a2905efbc33815e9e21ce16708e3eccffeb
Andifeind <andifeind@noname-media.com>
2016-08-22 22:29:01 +0200

    Examples

--- examples/syncmodel.js
+++ examples/syncmodel.js
@@ -2,6 +2,8 @@
 
 let CoreIO = require('../src/coreio');
 let log = require('logtopus').getLogger('coreio-example');
+let log2 = require('logtopus').getLogger('coreio');
+log2.setLevel('debug');
 let express = require('express');
 let app = express();
 
@@ -9,7 +11,8 @@ log.sys('Start sync model');
 
 let syncModel = new CoreIO.SyncModel('example', {
   defaults: {
-    counter: 0
+    counter: 0,
+    connections: 0
   }
 });
 
@@ -18,6 +21,14 @@ app.get('/count', (req, res) => {
   syncModel.set('counter', count += 1);
   res.send('OK');
 });
+
+syncModel.socket.__socket.on('connection', () => {
+  console.log('GOT sock', this);
+  syncModel.set('connections', syncModel.socket.__socket.connections.length)
+});
+
+
+app.listen(6446);
 
 log.sys(' ... listen on port:', syncModel.port);
 log.sys(' ... registered path:', syncModel.path);
