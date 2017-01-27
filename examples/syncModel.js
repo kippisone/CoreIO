621c6c217618dbad7e1777a51247ad8b0e265c91
Andifeind <andifeind@noname-media.com>
2017-01-27 23:54:35 +0100

    Changes

--- examples/syncModel.js
+++ examples/syncModel.js
@@ -1,12 +1,18 @@
 'use strict';
 
 const CoreIO = require('../src/coreio');
+const MongoDBService = require('coreio-mongodb')(CoreIO);
+
+CoreIO.setConf('mongodb', 'mongodb://localhost:27017/coreio-example');
 
 const fruitsModel = new CoreIO.SyncModel('fruits', {
   defaults: {
     banana: 'yellow',
     apple: 'red'
-  }
+  },
+
+  service: MongoDBService,
+  autoSave: true
 });
 
 fruitsModel.set('lemon', 'yellow');
